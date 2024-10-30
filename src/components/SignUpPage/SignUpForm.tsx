d3c536b889679470c6b49541d47c6aecbe59cb5e
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2024-10-30 11:31:07 +0100

    'Signup and signinform structure changed'

--- src/components/SignUpPage/SignUpForm.tsx
+++ src/components/SignUpPage/SignUpForm.tsx
@@ -1,26 +1,58 @@
+import React, { useState } from 'react';
 import './signUpScss/signUp.scss'
 
 const SignUpForm = () => {
+    const [isSignUp, setIsSignUp] = useState<boolean>(true);
 
+    const toggleForm = (): void => {
+        setIsSignUp(!isSignUp);
+    };
     return (
         <>
-        <div className="form-container">
-            <div>
-                <div className='text'>
-                    <h1>Sign Up</h1>
-                    <p>Welcome back!</p>
-                    <p>Enter details to proceed</p>
-                </div>
-                <form action="http://localhost:3000/signUpForm" method="POST" className='form'>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name="username" placeholder="Enter username"/>
+            <div className="form-container">
+                <div className='innerForm-container'>
+                    <div className='textDiv'>
+                        <h1>{isSignUp ? 'Sign Up' : 'Welcome Back'}</h1>
+                        <p>{isSignUp ? 'Enter details to create account' : 'Enter details to sign in'}</p>
+                    </div>
+                    <form
+                        action={
+                            isSignUp
+                                ? "http://localhost:5173/signUpForm"
+                                : "http://localhost:5173/signInForm"
+                        }
+                        method="POST"
+                        className="form"
+                    >
+                        <div className='inputs'>
+                            <input
+                                type="text"
+                                name="username"
+                                placeholder="Enter username"
+                                id="nameField"
+                                className={`usernameInput ${isSignUp ? '' : 'hidden'}`}
+                            />
+                            <input type="text" name="email" placeholder="Enter email" />
 
-                    <label htmlFor="password">Password</label>
-                    <input type="text" name="password" placeholder="Enter password"/>
-                    <button className='signUpBtn'>Sign Up</button>
-                </form>
+                            <input type="text" name="password" placeholder="Enter password" />
+
+                            {!isSignUp && <p className="forgotPsword">Forgot Password</p>}
+                        </div>
+
+                        <button type="submit" className="signUpBtn btn">
+                            {isSignUp ? 'Sign Up' : 'Sign In'} <span>&rarr;</span>
+                        </button>
+
+                        <div className="member">
+                            <p>{isSignUp ? 'Already have an account?' : "Don't have an account?"}</p>
+                            <button type="button" className="btn loginBtn" onClick={toggleForm}>
+                                {isSignUp ? 'Log in' : 'Sign up'}
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
             </div>
-        </div>
         </>
     );
 }
