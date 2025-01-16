aae15af0606ec1c97a711d69ee84798e2230e96a
valterlidesjo <valle.lidesjo@gmail.com>
2025-01-16 12:08:06 +0100

    explore

--- src/components/ExplorePage/ExplorePage.tsx
+++ src/components/ExplorePage/ExplorePage.tsx
@@ -1,6 +1,4 @@
 import SearchStock from "../exploreMainPage/exploreComponents/SearchStock";
-import HeaderPage from "../HeaderPage/HeaderPage";
-import StockSearch from "../StockSearch/StockSearch";
 import CategoriesExplore from "./ExplorePageComponents/CategoriesExplore";
 import CryptoExplore from "./ExplorePageComponents/CryptoExplore";
 import MarketsExplore from "./ExplorePageComponents/MarketsExplore";
